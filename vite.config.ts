4ea494635b9c1dd29a5d5d5f09f7fb0506ec5aae
qiangqiang-id <59910163+qiangqiang-id@users.noreply.github.com>
2024-10-31 14:53:18 +0800

    fix: 🐛 增加base

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/web-codecs-demo',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
